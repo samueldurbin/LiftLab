9ea35339bfcb85c6b4e8124f59a63c763ca488d2
Samuel Durbin <samuel.durbin@students.plymouth.ac.uk>
2024-10-22 16:26:27 +0100

    Add project files.

---
+++ LiftLab/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LiftLab
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
