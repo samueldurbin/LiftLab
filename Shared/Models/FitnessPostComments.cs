ddc2ecd41ece80e472076661fb2e2c4611cea98e
Samuel Durbin <samuel.durbin@students.plymouth.ac.uk>
2025-03-12 21:36:47 +0000

    shared class implemented

---
+++ Shared/Models/FitnessPostComments.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class FitnessPostComments
+    {
+        [Key] // primary key
+        public int FitnessPostCommentId { get; set; } // unique id fot each comment
+
+        public int FitnessPostId { get; set; } // forein key id for relavent fitnesspost
+
+        public string? Username { get; set; } // user who created the comment. at the minute its set to can be nullable for postman tests
+
+        public string Comment { get; set; } // the comment itself
+
+    }
+}
