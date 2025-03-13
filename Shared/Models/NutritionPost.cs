fe360a32a526139b9116b4f2750f72ee946b4b16
Samuel Durbin <samuel.durbin@students.plymouth.ac.uk>
2025-03-13 11:21:05 +0000

    NutritionPost class and controller/services added

---
+++ Shared/Models/NutritionPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class NutritionPost
+    {
+        public int NutritionPostId { get; set; } 
+        public string Username { get; set; }
+        public string ImageUrl { get; set; }
+        public string Caption { get; set; }
+        public int? MealPlanId { get; set; } // optional as not every post needs an added meal plan
+    }
+}
---
+++ WebApi/Controllers/NutritionPostsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [ApiController] // apicontroller
+    [Route("api/[controller]")] // url for api which uses the controller name as the url
+    public class NutritionPostsController : ControllerBase
+    {
+        private readonly INutritionPostService _nutritionPostService; // dependency injection
+
+        public NutritionPostsController(INutritionPostService nutritionPostService) // creates an instance of the IService
+        {
+            _nutritionPostService = nutritionPostService;
+        }
+
+    }
+}
