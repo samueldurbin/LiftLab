d01b1d52fd2420c6e3e5b55ead93365f36d8f563
Samuel Durbin <samuel.durbin@students.plymouth.ac.uk>
2025-03-22 12:13:38 +0000

    Title and First Box in View

--- LiftLab/Views/FitnessPage.xaml.cs
+++ LiftLab/Views/FitnessPage.xaml.cs
@@ -6,12 +6,5 @@ public partial class FitnessPage : ContentPage
 	{
 		InitializeComponent();
 	}
-    private async void OnCreateClicked(object sender, EventArgs e) // simple navigation for creating workout
-    {
-        await Navigation.PushAsync(new CreateWorkoutPage());
-    }
-    private async void OnViewClicked(object sender, EventArgs e) // simple navigation for testing
-    {
-        await Navigation.PushAsync(new CreateWorkoutPage());
-    }
+    
 }
