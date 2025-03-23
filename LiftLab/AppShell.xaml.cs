70a46498757712828e71f588d26c683483babfbe
Samuel Durbin <samuel.durbin@students.plymouth.ac.uk>
2025-03-23 17:12:34 +0000

    Settings Page Implementation

--- LiftLab/AppShell.xaml.cs
+++ LiftLab/AppShell.xaml.cs
@@ -15,6 +15,7 @@ namespace LiftLab
             Routing.RegisterRoute(nameof(NutritionHomePage), typeof(NutritionHomePage));
             Routing.RegisterRoute(nameof(FriendsPage), typeof(FriendsPage));
             Routing.RegisterRoute(nameof(ViewAllWorkoutPlans), typeof(ViewAllWorkoutPlans));
+            Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
 
         }
     }
---
+++ LiftLab/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiftLab.Views;
+using System.Windows.Input;
+using LiftLab.Services;
+using Shared.Models;
+using System.Collections.ObjectModel;
+
+namespace LiftLab.ViewModels
+{
+    public class ProfileViewModel : BaseViewModel
+    {
+        public ICommand SettingsCommand { get; }
+        public ProfileViewModel()
+        {
+            SettingsCommand = new Command(async () => // add button in the ui navigates to create a post
+            {
+                await Shell.Current.GoToAsync(nameof(SettingsPage));
+            });
+        }
+    }
+}
