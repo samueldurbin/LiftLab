225251378ae10f5f20dd23aa5c056e908dcfc023
Samuel Durbin <samuel.durbin@students.plymouth.ac.uk>
2025-04-22 10:30:03 +0100

    CommunityPostServiceUI

--- LiftLab/Services/CommunityPostServiceUI.cs
+++ LiftLab/Services/CommunityPostServiceUI.cs
@@ -2,30 +2,33 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LiftLab.Services
 {
-    public class CommunityServiceUI
+    public class CommunityPostServiceUI
     {
-        private readonly HttpClient _httpClient;
-        public CommunityServiceUI()
+        // this will be used to sned Http requests and recieve respomses from the uri
+        private readonly HttpClient _httpClient; // readonly instance of HttpClient
+
+        public CommunityPostServiceUI()
         {
-            _httpClient = new HttpClient // Creates an instance of HTTP Client
+            _httpClient = new HttpClient // creates an instance of HTTP Client
             {
                 BaseAddress = new Uri("https://web.socem.plymouth.ac.uk/COMP3000/SDurbin/api/") // URL for api requests
             };
         }
 
-        #region CreatePost
+        #region Create Post
 
         // this method creates a community post (either fitness or nutritional) using the user input and sends to the database through the below api post request
         // userid is the user creating the post which has preferences in the frontend to associate each created post with the logged in user
         public async Task<CommunityPost> CreatePost(int userId, string username, string caption, int? workoutPlanId, int? mealId, int? mealPlanId) // workoutplanid, mealplanid, and mealid are all nullable objects so users can select what they want
         {
-            // sends a http get request to the communityposts create endpoint
+        
             var communityPost = await _httpClient.PostAsJsonAsync("CommunityPosts/createpost", new CommunityPost // creates a new CommunityPost object to send to backend
             {
                 UserId = userId, // who is creating the post
@@ -39,14 +42,13 @@ namespace LiftLab.Services
 
             if (communityPost.IsSuccessStatusCode) // this checks if the http post request was successful
             {
-                return await communityPost.Content.ReadFromJsonAsync<CommunityPost>(); // returns communitypost object if successful
+                return await communityPost.Content.ReadFromJsonAsync<CommunityPost>(); // returns post object if successful
             }
 
             return null; // this would return null is the createpost request was not successful
         }
 
         #endregion
-
 
         #region Get Posts & Plans
 
@@ -61,11 +63,11 @@ namespace LiftLab.Services
                 return await communityPosts.Content.ReadFromJsonAsync<List<CommunityPost>>(); // returns the json body and deserializes the json into a communitypost object
             }
 
-            throw new Exception("Failed to get community posts, please try again!"); // failed to retrieve the posts exception message
+            throw new Exception("Failed to get posts, please try again!"); // failed to retrieve the posts exception message
         }
 
-        // this method gets the workoutplans that were created/added by a user (not requesting every workoutplan)
-        // uses the userId to retrieve a list of workoutplans
+        // this method gets the workout plans that were created/added by a user (not requesting every workout plan)
+        // uses the userId to retrieve a list of workout plans
         public async Task<List<WorkoutPlans>> GetWorkoutPlansByUserId(int userId)
         {
             var workoutPlans = await _httpClient.GetAsync($"WorkoutPlans/getworkoutplansbyuser/{userId}"); // sends a http get request to the api, to retrieve the list of plans
@@ -78,12 +80,12 @@ namespace LiftLab.Services
             throw new Exception("Failed to get workout plans, please try again!");
         }
 
-        // this method gets all the mealplans created/added by a userid and retrieves them as a list
+        // this method gets all the meal plans created/added by a userid and retrieves them as a list
         public async Task<List<MealPlans>> GetMealPlansByUserId(int userId)
         {
-            var mealPlans = await _httpClient.GetAsync($"MealPlans/getmealplansbyuser/{userId}"); // sends a http get request to the api, to retrieve the list of mealplans
+            var mealPlans = await _httpClient.GetAsync($"MealPlans/getmealplansbyuser/{userId}"); // sends a http get request to the api, to retrieve the list of meal plans
 
-            if (mealPlans.IsSuccessStatusCode)
+            if (mealPlans.IsSuccessStatusCode) // checks for success
             {
                 return await mealPlans.Content.ReadFromJsonAsync<List<MealPlans>>(); // shows a 200 code if success
             }
@@ -91,73 +93,25 @@ namespace LiftLab.Services
             throw new Exception("Failed to get workout plans, please try again!"); // exception message
         }
 
-        // this method gets all the individual meals from the database
+        // this method gets all the individual meals that an individual user has created from the database
         // returns them as a list
-        public async Task<List<Meals>> GetMealsByUserId(int userId)
+        public async Task<List<Meals>> GetMealsByUser(int userId)
         {
-            var meals = await _httpClient.GetAsync($"MealPlans/meals/{userId}"); // sends a http get request with the input userId
+            var meals = await _httpClient.GetAsync($"MealPlans/meals/{userId}"); // sends a http get request to the api, to retrieve a list of individual meals created by a user
 
-            if (meals.IsSuccessStatusCode)
+            if (meals.IsSuccessStatusCode) // checks for success
             {
                 return await meals.Content.ReadFromJsonAsync<List<Meals>>();
             }
 
-            throw new Exception("Failed to get meals, please try again.");
+            throw new Exception("Failed to get meals, please try again!");
         }
 
         #endregion
-
-
-        #region Comments
-        public async Task<bool> CreateComment(int postId, string username, string comment)
-        {
-            var commentDto = new AddNewCommentDTO // a dto to match the api request
-            {
-                CommunityPostId = postId, // the fitnesspost that the comment is being added on
-                Username = username, // the user who is adding the comment
-                Comment = comment // the comment
-            };
-
-            var response = await _httpClient.PostAsJsonAsync("CommunityPosts/addcomment", commentDto); // http post request to add comment to fitnesspost
-
-            return response.IsSuccessStatusCode;
-        }
-
-        public async Task<List<CommunityPostComments>> GetCommentsByPost(int postId)
-        {
-            var response = await _httpClient.GetAsync($"CommunityPosts/comments/{postId}"); // recieves comments from a specific post
-
-            if (response.IsSuccessStatusCode) // this helps postman request testing
-            {
-                return await response.Content.ReadFromJsonAsync<List<CommunityPostComments>>(); 
-            }
-
-            return new List<CommunityPostComments>(); // returns a lsit of fitnesspost comments
-        }
-
-        #endregion
-
-
-        #region Post Liking
-        public async Task<bool> LikePost(int postId, int userId)
-        {
-            var response = await _httpClient.PostAsync($"CommunityPosts/like/{postId}/{userId}", null); // sends a http post request and not sending a body (only uses parameters)
-            return response.IsSuccessStatusCode;
-        }
-        #endregion
-
-        public async Task<List<Meals>> GetMealsByUser(int userId)
-        {
-            var response = await _httpClient.GetAsync($"MealPlans/meals/{userId}");
-            if (response.IsSuccessStatusCode)
-            {
-                return await response.Content.ReadFromJsonAsync<List<Meals>>();
-            }
-            throw new Exception("Failed to get meals for the user");
-        }
 
         #region Adding External Plans and Meals
 
+        // adds a workout plan created by another user to the logged in users account
         public async Task<bool> AddExternalUserWorkoutPlan(int planId, int userId)
         {
             var externalWorkoutPlan = await _httpClient.PostAsync($"WorkoutPlans/adduserworkoutplan/{planId}/{userId}", null); // sends a http post request with the ids
@@ -170,6 +124,7 @@ namespace LiftLab.Services
             throw new Exception("Failed to add workout plan to user, please try again!"); // exception message
         }
 
+        // adds a meal plan created by another user to the logged in users account
         public async Task<bool> AddExternalUserMealPlans(int mealPlanId, int userId)
         {
             var externalMealPlan = await _httpClient.PostAsync($"MealPlans/addusermealplan/{mealPlanId}/{userId}", null); // sends a http post request with the ids
@@ -182,6 +137,7 @@ namespace LiftLab.Services
             throw new Exception("Failed to add workout plan to user, please try again!"); // exception message
         }
 
+        // adds an indiviual meal created by another user to the logged in users account
         public async Task<bool> AddExternalUserMeals(int mealId, int userId)
         {
             var externalMeals = await _httpClient.PostAsync($"MealPlans/addusermeal/{mealId}/{userId}", null); // sends a http post request with the ids
@@ -194,18 +150,18 @@ namespace LiftLab.Services
             throw new Exception("Failed to add workout plan to user, please try again!"); // exception message
         }
 
-
-        public async Task<bool> AddExternalPlans(CommunityPost post, int userId) // instance of a communitypost with a userid
+        // this method takes all the adding external plans methods into a check, so that the there can be one universal add button on each post
+        public async Task<bool> AddExternalPlans(CommunityPost post, int userId) // instance of a community post with a userid
         {
-            if (post.WorkoutPlanId != null) // checks if a community post has a workoutplan attached
+            if (post.WorkoutPlanId != null) // checks if the community post has a workout plan attached
             {
                 return await AddExternalUserWorkoutPlan(post.WorkoutPlanId.Value, userId); // if it does, call this method
             }
-            else if (post.MealPlanId != null) // checks if a community post has a mealplan attached
+            else if (post.MealPlanId != null) // checks if the community post has a meal plan attached
             {
                 return await AddExternalUserMealPlans(post.MealPlanId.Value, userId);
             }
-            else if (post.MealId != null) // checks if a community post has a meal attached
+            else if (post.MealId != null) // checks if the community post has a meal attached
             {
                 return await AddExternalUserMeals(post.MealId.Value, userId);
             }
@@ -215,14 +171,58 @@ namespace LiftLab.Services
 
         #endregion
 
+        #region Comments
+
+        // creates a comment using a request body
+        // using this dto reduces payload size
+        public async Task<bool> CreateComment(int postId, string username, string comment)
+        {
+            var commentDto = new AddNewCommentDTO // a dto to match the api request
+            {
+                CommunityPostId = postId, // the post that the comment is being added on
+                Username = username, // the user who is adding the comment
+                Comment = comment // the comment
+            };
+
+            var addComment = await _httpClient.PostAsJsonAsync("CommunityPosts/addcomment", commentDto); // http post request to add comment to post
+
+            return addComment.IsSuccessStatusCode;
+        }
+
+        // gets all the comments by the post
+        public async Task<List<CommunityPostComments>> GetCommentsByPost(int postId) // returns list of comments
+        {
+            var comments = await _httpClient.GetAsync($"CommunityPosts/comments/{postId}"); // receives comments from a specific post
+
+            if (comments.IsSuccessStatusCode) // checks for success
+            {
+                return await comments.Content.ReadFromJsonAsync<List<CommunityPostComments>>(); // retrieves list of comments
+            }
+
+            return new List<CommunityPostComments>(); // returns an empty list of comments to ensure app doesnt crash if a post hasnt been commented on
+        }
+
+        #endregion
+
+        #region Post Liking
+
+        public async Task<bool> LikePost(int postId, int userId) // the postid and the user liking the post
+        {
+            var like = await _httpClient.PostAsync($"CommunityPosts/like/{postId}/{userId}", null); // sends a http post request and not sending a body (only uses parameters)
+
+            return like.IsSuccessStatusCode;
+        }
+
+        #endregion
 
         #region Profile
 
+        // gets user for profile viewing
         public async Task<Users> GetUserById(int userId)
         {
             var response = await _httpClient.GetAsync($"Users/getuserbyid/{userId}"); // aoi endpoint with the inserted userId
 
-            if (response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode) // checks for success
             {
                 return await response.Content.ReadFromJsonAsync<Users>();
             }
@@ -230,13 +230,12 @@ namespace LiftLab.Services
             return null;
         }
 
-        #endregion
-
+        // this gets all the posts created by the user
         public async Task<List<CommunityPost>> GetCommunityPostsByUserId(int userId)
         {
-            var response = await _httpClient.GetAsync($"CommunityPosts/getpostsbyuser/{userId}");
+            var response = await _httpClient.GetAsync($"CommunityPosts/getpostsbyuser/{userId}"); // api get request with userid input
 
-            if (response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode) // checks for success
             {
                 return await response.Content.ReadFromJsonAsync<List<CommunityPost>>();
             }
@@ -249,7 +248,9 @@ namespace LiftLab.Services
             var response = await _httpClient.DeleteAsync($"CommunityPosts/deletepost/{postId}/{userId}"); //http delete request
             return response.IsSuccessStatusCode;
         }
+
+        #endregion
+
+
     }
-
 }
-
--- Shared/Models/AddNewCommentDTO.cs
+++ Shared/Models/AddNewCommentDTO.cs
@@ -9,7 +9,6 @@ namespace Shared.Models
     // this is a Data Transfer Object for Comments in order to decrease the request body size in backend testing
     public class AddNewCommentDTO
     {
-        //public int FitnessPostCommentId { get; set; }
         public int CommunityPostId { get; set; }
         public string Username { get; set; }
         public string Comment { get; set; }
