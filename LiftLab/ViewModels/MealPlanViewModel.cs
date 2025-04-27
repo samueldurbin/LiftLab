9cc0ddb6cd38c8d439b7f3e7e9e0371d72234f42
Samuel Durbin <samuel.durbin@students.plymouth.ac.uk>
2025-04-27 01:09:04 +0100

    Viewing Meals in Meal Plan

--- LiftLab/ViewModels/MealPlanViewModel.cs
+++ LiftLab/ViewModels/MealPlanViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Shared.Models;
+﻿using LiftLab.Views;
+using Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LiftLab.ViewModels
 {
@@ -28,5 +30,23 @@ namespace LiftLab.ViewModels
         public string MealPlanName => MealPlan?.MealPlanName;
         public DateTime CreatedAt => MealPlan?.CreatedAt ?? DateTime.MinValue;
         public ICollection<Meals> Meals => MealPlan?.Meals ?? new List<Meals>();
+
+        public ICommand NavigateToMealDetailsCommand { get; }
+
+        public MealPlanViewModel()
+        {
+            NavigateToMealDetailsCommand = new Command<Meals>(async (meal) => await NavigateToMealDetails(meal));
+        }
+
+        private async Task NavigateToMealDetails(Meals meal)
+        {
+            if (meal == null)
+                return;
+
+            await Shell.Current.GoToAsync(nameof(ViewMealsPage), true, new Dictionary<string, object>
+            {
+                { "Meal", meal }
+            });
+        }
     }
 }
